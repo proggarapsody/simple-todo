e5be9639b65ff60795651765b8d118da91caa8d5
Aleksey Klimenko <klimenkoaleksey.prog@gmail.com>
2022-05-30 22:07:26 +0300

    feat: delete tag selector, besides global styling

--- app.js
+++ app.js
@@ -9,12 +9,13 @@
 
 var taskInput = document.getElementById('new-task'); //Add a new task.
 var addButton = document.getElementsByTagName('button')[0]; //first button
-var incompleteTaskHolder = document.getElementById('incompleteTasks'); //ul of #incompleteTasks
+var incompleteTaskHolder = document.getElementById('incomplete-tasks'); //ul of #incompleteTasks
 var completedTasksHolder = document.getElementById('completed-tasks'); //completed-tasks
 
 //New task list item
 var createNewTaskElement = function (taskString) {
   var listItem = document.createElement('li');
+  listItem.classList.add('task-container');
 
   //input (checkbox)
   var checkBox = document.createElement('input'); //checkbx
@@ -30,7 +31,8 @@ var createNewTaskElement = function (taskString) {
   var deleteButtonImg = document.createElement('img'); //delete button image
 
   label.innerText = taskString;
-  label.className = 'task';
+  label.className = 'task-label';
+  checkBox.className = 'task-checkbox';
 
   //Each elements, needs appending
   checkBox.type = 'checkbox';
@@ -77,7 +79,7 @@ var editTask = function () {
   var editInput = listItem.querySelector('input[type=text]');
   var label = listItem.querySelector('label');
   var editBtn = listItem.querySelector('.edit');
-  var containsClass = listItem.classList.contains('editMode');
+  var containsClass = listItem.classList.contains('edit-mode');
   //If class of the parent is .editmode
   if (containsClass) {
     //switch to .editmode
@@ -90,7 +92,7 @@ var editTask = function () {
   }
 
   //toggle .editmode on the parent.
-  listItem.classList.toggle('editMode');
+  listItem.classList.toggle('edit-mode');
 };
 
 //Delete task.
